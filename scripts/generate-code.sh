f16541831df44fa846579679982ac2ed64858f2e
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2025-01-05 18:49:01 -0500

    Add workflow for generating code.

--- scripts/generate-code.sh
+++ scripts/generate-code.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-
+mkdir temp-source && cd temp-source
+openapi-generator-cli generate -g rust -i ../openapi-spec.yaml && \
+    rm -rf ../src ../docs &&
+    mv Cargo.toml README.md src docs ../ && \
+    cd .. && \
+    rm -r temp-source
