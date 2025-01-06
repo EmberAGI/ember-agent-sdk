428b5a4261a7446e8adbd38d1817dd2653895fb1
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2025-01-05 19:00:11 -0500

    Do not change cargo toml

--- scripts/generate-code.sh
+++ scripts/generate-code.sh
@@ -3,6 +3,6 @@
 mkdir temp-source && cd temp-source
 openapi-generator-cli generate -g rust -i ../openapi-spec.yaml && \
     rm -rf ../src ../docs &&
-    mv Cargo.toml README.md src docs ../ && \
+    mv README.md src docs ../ && \
     cd .. && \
     rm -r temp-source
